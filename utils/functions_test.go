c6ddeef5e6035270104e8c96bc2f635752abd866
Nedyalko Andreev <nedyalko.andreev@gmail.com>
2015-08-11 15:50:24 +0300

    Fix formatting

--- utils/functions_test.go
+++ utils/functions_test.go
@@ -58,13 +58,11 @@ func TestProperEnvironmentCreation(t *testing.T) {
 		t.Fatal("Was not able to find the current user")
 	}
 
-
 	cfg := getCfg(config.SystemSection{
 		User:    currentUser.Username,
 		Workdir: tempDir,
 		Pidfile: tempFile,
 	})
-
 
 	if err := SetupEnv(cfg); err != nil {
 		t.Fatalf("Error on creating environment. %s", err)

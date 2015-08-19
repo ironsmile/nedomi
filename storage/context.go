c02748c3df1296c7be1d6a88c105fb82f24c3d21
Nedyalko Andreev <nedyalko.andreev@gmail.com>
2015-08-19 15:05:40 +0300

    Rename some vars

--- storage/context.go
+++ storage/context.go
@@ -8,15 +8,15 @@ type storageContextKey int
 
 const sKey storageContextKey = 0
 
-// NewContext returns a new Context carrying the map with the supplied storages
-func NewContext(ctx context.Context, cms map[string]Storage) context.Context {
-	return context.WithValue(ctx, sKey, cms)
+// NewContext returns a new Context carrying the map with the supplied storages.
+func NewContext(ctx context.Context, storages map[string]Storage) context.Context {
+	return context.WithValue(ctx, sKey, storages)
 }
 
 // FromContext extracts the map of storage.Storage objects, if present.
 func FromContext(ctx context.Context) (map[string]Storage, bool) {
 	// ctx.Value returns nil if ctx has no value for the key;
 	// the map[string]storage.Storage type assertion returns ok=false for nil.
-	cms, ok := ctx.Value(sKey).(map[string]Storage)
-	return cms, ok
+	storages, ok := ctx.Value(sKey).(map[string]Storage)
+	return storages, ok
 }

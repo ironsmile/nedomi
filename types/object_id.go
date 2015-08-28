460a72112833c353219004cb5f1357378ec226ca
Nedyalko Andreev <nedyalko.andreev@gmail.com>
2015-08-28 12:14:43 +0300

    Add hashing directly in the ObjectID type

--- types/object_id.go
+++ types/object_id.go
@@ -1,14 +1,40 @@
 // Package types describes few of the essential types used throughout the application.
 package types
 
-import "fmt"
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
+	"io"
+)
+
+//!TODO: maybe make CacheKey and Path private and add getters for them? It would
+// prevent changes in their value after the hash has already been calculated. If
+// we want to be albe to change them, we can add setters that reset the hash.
 
 // ObjectID represents a cached file
 type ObjectID struct {
 	CacheKey string
 	Path     string
+	hash     []byte
+	//!TODO: add vary headers information
 }
 
-func (oid ObjectID) String() string {
-	return fmt.Sprintf("%s:%s", oid.CacheKey, oid.Path)
+func (oid *ObjectID) String() string {
+	return fmt.Sprintf("%x:%s:%s", oid.Hash(), oid.CacheKey, oid.Path)
+}
+
+// StrHash returns the sha1 hash of the selected object id in hex format.
+func (oid *ObjectID) StrHash() string {
+	return hex.EncodeToString(oid.Hash())
+}
+
+// Hash returns the sha1 hash of the selected object id.
+func (oid *ObjectID) Hash() []byte {
+	if len(oid.hash) == 0 {
+		h := sha1.New()
+		io.WriteString(h, oid.CacheKey+"/"+oid.Path)
+		oid.hash = h.Sum(nil)
+	}
+	return oid.hash
 }
--- types/object_id_test.go
+++ types/object_id_test.go
@@ -7,6 +7,19 @@ import (
 	"fmt"
 )
 
+func TestObjectIDHash(t *testing.T) {
+	obj := &ObjectID{
+		CacheKey: "1.2",
+		Path:     "/somewhere",
+	}
+
+	res := obj.StrHash()
+	expected := "583fae38a17840864d328e08b0d21cec293f74b2"
+	if expected != res {
+		t.Errorf("Incorrect ObjectID hash. Exected %s, got %s", expected, res)
+	}
+}
+
 func TestObjectIDStringersWithSensibleData(t *testing.T) {
 	obj := &ObjectID{
 		CacheKey: "1.2",
@@ -25,6 +38,10 @@ func TestObjectIDStringersWithSensibleData(t *testing.T) {
 
 	if !strings.Contains(result, obj.Path) {
 		t.Errorf("The result '%s' does not contain the path '%s'", result, obj.Path)
+	}
+
+	if !strings.Contains(result, obj.StrHash()) {
+		t.Errorf("The result '%s' does not contain the hash '%s'", result, obj.StrHash())
 	}
 
 }

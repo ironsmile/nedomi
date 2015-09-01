1c0559e43263648128227d752a8dccced0ecc3a8
Nedyalko Andreev <nedyalko.andreev@gmail.com>
2015-09-01 19:38:28 +0300

    Simplify and fix simple upstream tests

--- upstream/simple/impl_test.go
+++ upstream/simple/impl_test.go
@@ -1,81 +1,49 @@
-package simple_test
+package simple
 
 import (
-	"bytes"
-	"io"
-	"io/ioutil"
-	"net"
+	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"net/url"
-	"os"
 	"testing"
-	"time"
-
-	"github.com/ironsmile/nedomi/config"
-	"github.com/ironsmile/nedomi/upstream/simple"
 )
 
-func TestGetRequest(t *testing.T) {
-	var b = "Hello world is an awesome test string"
-	var start, end uint64 = 3, 500000
-	file, err := ioutil.TempFile(os.TempDir(), "test")
+func TestSimpleUpstream(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/err" {
+			w.WriteHeader(404)
+			fmt.Fprint(w, "error!")
+			return
+		}
+		w.WriteHeader(200)
+		fmt.Fprint(w, "hello world")
+	}))
+	defer ts.Close()
+
+	upstreamURL, err := url.Parse(ts.URL)
 	if err != nil {
 		t.Fatal(err)
 	}
-	_, err = io.WriteString(file, b)
+	upstream := New(upstreamURL)
+
+	req1, err := http.NewRequest("GET", "http://www.somewhere.com/err", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-	file.Sync()
-	listener, err := net.Listen("tcp", "localhost:0")
+	resp1 := httptest.NewRecorder()
+	upstream.ServeHTTP(resp1, req1)
+	if resp1.Code != 404 || resp1.Body.String() != "error!" {
+		t.Errorf("Unexpected response %#v", resp1)
+	}
+
+	req2, err := http.NewRequest("GET", "http://www.somewhere.com/index", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-	go func() {
-		http.Serve(listener, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			http.ServeContent(w, r, "", time.Time{}, file)
-		}))
-	}()
-
-	cz := &config.CacheZoneSection{
-		ID:             "42",
-		Path:           "/var/tmp",
-		StorageObjects: 1024,
-		PartSize:       2,
-	}
-	var m = make(map[string]*config.CacheZoneSection)
-	m[cz.ID] = cz
-
-	vh := new(config.VirtualHost)
-	vh.Name = "localhost"
-	vh.HandlerType = "proxy"
-	vh.CacheKey = "test"
-	vh.UpstreamType = "simple"
-	vh.UpstreamAddress, _ = url.Parse("http://" + listener.Addr().String())
-	vh.CacheZone = cz
-	if err := vh.Validate(); err != nil {
-		t.Fatal(err)
+	resp2 := httptest.NewRecorder()
+	upstream.ServeHTTP(resp2, req2)
+	if resp2.Code != 200 || resp2.Body.String() != "hello world" {
+		t.Errorf("Unexpected response %#v", resp2)
 	}
 
-	upstreamURL, err := url.Parse("http://" + listener.Addr().String())
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	u := simple.New(upstreamURL)
-
-	resp, err := u.GetRequestPartial(file.Name(), start, end)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
-	result, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected := []byte(b)[start:]
-	if !bytes.Equal(expected, result) {
-		t.Fatalf("Expected [%s] got [%s]", expected, result)
-	}
 }

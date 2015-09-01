4644d953f43a0b50da982a4b77028b327e3f0985
Nedyalko Andreev <nedyalko.andreev@gmail.com>
2015-09-01 19:10:36 +0300

    Minor fix and rename to follow go conventions

--- logger/new_test.go
+++ logger/new_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestCreatingBogusLogger(t *testing.T) {
-	_, err := New(config.LoggerSection{Type: "bogus_logger"})
+	_, err := New(&config.LoggerSection{Type: "bogus_logger"})
 
 	if err == nil {
 		t.Error("There was no error when creating bogus logger")

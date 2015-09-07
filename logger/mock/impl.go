f77476d2f6532cee0a1c5b61760a263be1d2da87
Mihail Stoykov <M.Stoikov@gmail.com>
2015-09-07 19:39:22 +0300

    logger.mock: fix comments and make constants public

--- logger/mock/impl.go
+++ logger/mock/impl.go
@@ -22,24 +22,24 @@ type Mock struct {
 func (b *Mock) log(arg string) {
 	b.Lock()
 	defer b.Unlock()
-	b.slice = append(b.slice, fmt.Sprint(logPrefix, arg))
+	b.slice = append(b.slice, fmt.Sprint(LogPrefix, arg))
 }
 
 func (b *Mock) debug(arg string) {
 	b.Lock()
 	defer b.Unlock()
-	b.slice = append(b.slice, fmt.Sprint(debugPrefix, arg))
+	b.slice = append(b.slice, fmt.Sprint(DebugPrefix, arg))
 }
 func (b *Mock) err(arg string) {
 	b.Lock()
 	defer b.Unlock()
-	b.slice = append(b.slice, fmt.Sprint(errorPrefix, arg))
+	b.slice = append(b.slice, fmt.Sprint(ErrorPrefix, arg))
 }
 func (b *Mock) fatal(arg string) {
 	b.Lock()
 	defer b.Unlock()
-	b.slice = append(b.slice, fmt.Sprint(fatalPrefix, arg))
-	panic("Fatal called")
+	b.slice = append(b.slice, fmt.Sprint(FatalPrefix, arg))
+	panic(FatalPanicText)
 }
 
 // Logged returns a slice of strings that is everything that has been logged
@@ -59,49 +59,53 @@ func (b *Mock) Clear() {
 	b.slice = make([]string, 0)
 }
 
-// Log is the same as log.Println if level is atleast 'info'
+// Log logs a message with fmt.Sprint with LogPrefix infront
 func (b *Mock) Log(args ...interface{}) {
 	b.log(fmt.Sprint(args...))
 }
 
-// Logf is the same as log.Printf, with a '\n' at the end of format if missing, if level is atleast 'info'
+// Logf logs a message with fmt.Sprintf with LogPrefix infront
 func (b *Mock) Logf(format string, args ...interface{}) {
 	b.log(fmt.Sprintf(format, args...))
 }
 
-// Debug is the same as log.Println if level is atleast 'debug'
+// Debug logs a message with fmt.Sprint adding DebugPrefix infront
 func (b *Mock) Debug(args ...interface{}) {
 	b.debug(fmt.Sprint(args...))
 }
 
-// Debugf is the same as log.Printf, with a '\n' at the end of format if missing, if level is atleast 'debug'
+// Debugf logs a message with fmt.Sprintf adding DebugPrefix infront
 func (b *Mock) Debugf(format string, args ...interface{}) {
 	b.debug(fmt.Sprintf(format, args...))
 }
 
-// Error is the same as log.Println if level is atleast 'error'
+// Error logs a message with fmt.Sprint adding ErrorPrefix infront
 func (b *Mock) Error(args ...interface{}) {
 	b.err(fmt.Sprint(args...))
 }
 
-// Errorf is the same as log.Printf, with a '\n' at the end of format if missing, if level is atleast 'error'
+// Errorf logs a message with fmt.Sprintf adding ErrorPrefix infront
 func (b *Mock) Errorf(format string, args ...interface{}) {
 	b.err(fmt.Sprintf(format, args...))
 }
 
-// Fatal is the same as log.Fatalln if level is atleast 'fatal'
+// Fatal logs a message with fmt.Sprint adding FatalPrefix infront and then panics
 func (b *Mock) Fatal(args ...interface{}) {
 	b.fatal(fmt.Sprint(args...))
 }
 
-// Fatalf is the same as log.Fatalf, with a '\n' at the end of format if missing, if level is atleast 'fatal'
+// Fatalf logs a message with fmt.Sprintf adding FatalPrefix infront and then panics
 func (b *Mock) Fatalf(format string, args ...interface{}) {
 	b.fatal(fmt.Sprintf(format, args...))
 }
 
+// Prefixes for the logged messages
 const (
-	fatalPrefix = "Fatal:"
-	errorPrefix = "Error:"
-	logPrefix   = "Log:"
-	debugPrefix = "Debug:"
+	FatalPrefix = "Fatal:"
+	ErrorPrefix = "Error:"
+	LogPrefix   = "Log:"
+	DebugPrefix = "Debug:"
 )
+
+// FatalPanicText is the text of the panic when Fatalf? is called
+const FatalPanicText = "Fatal called"

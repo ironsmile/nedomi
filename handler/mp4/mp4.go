cd98edcfbecad62b125d26b87ed8d410d02343e6
Mihail Stoykov <M.Stoikov@gmail.com>
2015-09-16 18:45:11 +0300

    use the mp4.Decode from the module

--- handler/mp4/mp4.go
+++ handler/mp4/mp4.go
@@ -73,7 +73,13 @@ func (m *mp4Handler) RequestHandle(ctx context.Context, w http.ResponseWriter, r
 	removeQueryArgument(newreq.URL, startKey)
 
 	var rr = &rangeReader{ctx: ctx, req: copyRequest(r), location: l, next: m.next}
-	var video = m.mapMp4File(rr)
+	var video *mp4.MP4
+	video, err = mp4.Decode(rr)
+	if err != nil {
+		m.logger.Errorf("error from the mp4.Decode - %s", err)
+		m.next.RequestHandle(ctx, w, r, l)
+		return
+	}
 	if video == nil || video.Moov == nil { // missing something?
 		m.next.RequestHandle(ctx, w, r, l)
 		return
@@ -119,63 +125,7 @@ func (rr *rangeReader) Range(start, length uint64) io.ReadCloser {
 
 	return in
 }
+
 func (rr *rangeReader) RangeRead(start, length uint64) (io.ReadCloser, error) {
 	return rr.Range(start, length), nil
-}
-
-func (m *mp4Handler) mapMp4File(rr *rangeReader) *mp4.MP4 {
-	video := &mp4.MP4{}
-	var in = rr.Range(0, firstRequestSize)
-	var currentOffset uint64
-	var leftFromCurrentRequest uint64 = firstRequestSize
-
-	for {
-		var boxes []mp4.Box
-		h, err := mp4.DecodeHeader(in)
-		if err != nil {
-			if err == io.EOF {
-				return video
-			}
-			m.logger.Error(err)
-			return nil
-		}
-		if h.Type == "mdat" { // don't decode that now
-			m.logger.Debugf("got mdat(%d) at %d", h.Size, currentOffset)
-			video.Mdat = &mp4.MdatBox{Offset: currentOffset, ContentSize: mp4.RemoveHeaderSize(h.Size)}
-			if video.Moov != nil { // done
-				return video
-			}
-			currentOffset += h.Size
-			leftFromCurrentRequest = firstRequestSize
-			in.Close()
-
-			in = rr.Range(currentOffset, firstRequestSize)
-
-			continue
-		}
-		requiredFromRequest := (h.Size + mp4.HeaderSizeFor(h.Size))
-		if requiredFromRequest > leftFromCurrentRequest {
-			var start, length = currentOffset + leftFromCurrentRequest, requiredFromRequest - leftFromCurrentRequest + firstRequestSize
-			leftFromCurrentRequest += length
-			in = utils.MultiReadCloser(in, rr.Range(start, length))
-		}
-		box, err := mp4.DecodeBox(h, in)
-		if err != nil {
-			m.logger.Error(err)
-			return nil
-		}
-		switch h.Type {
-		case "ftyp":
-			m.logger.Debugf("got ftyp(%d) at %d", h.Size, currentOffset)
-			video.Ftyp = box.(*mp4.FtypBox)
-		case "moov":
-			m.logger.Debugf("got moov(%d) at %d", h.Size, currentOffset)
-			video.Moov = box.(*mp4.MoovBox)
-		default:
-			m.logger.Debugf("got a box(%d) at %d", h.Size, currentOffset)
-			boxes = append(boxes, box) // not actually written in the output - hope it's not interesting :P
-		}
-		leftFromCurrentRequest -= h.Size
-		currentOffset += h.Size
-	}
 }

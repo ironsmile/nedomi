ed1b461e3b2ba95c50601cb653a1db261155f464
Mihail Stoykov <M.Stoikov@gmail.com>
2015-09-23 13:02:39 +0300

    upstream.mock: don't use pointers to functions

--- upstream/mock.go
+++ upstream/mock.go
@@ -14,14 +14,14 @@ const MockDefaultResponse = "Hello"
 // NewMock creates and returns a new http.ServeMux instance that serves as a
 // mock upstream. The default handler can be specified. If nil, a simple handler
 // that always returns 200 and "Hello" is used.
-func NewMock(defaultHandler *http.HandlerFunc) *http.ServeMux {
+func NewMock(defaultHandler http.HandlerFunc) *http.ServeMux {
 	//!TODO: maybe use a custom type instaed of http.ServeMux directly. That way
 	// we can add extra helper functions like delayed handling, responce copiers
 	// and other goodies.
 
 	upstream := http.NewServeMux()
 	if defaultHandler != nil {
-		upstream.HandleFunc("/", *defaultHandler)
+		upstream.HandleFunc("/", defaultHandler)
 	} else {
 		upstream.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(MockDefaultResponseCode)

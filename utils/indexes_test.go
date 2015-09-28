d8e76992b4b9228baf768f5b2d5e4ed601c8975b
Nedyalko Andreev <nedyalko.andreev@gmail.com>
2015-09-28 14:45:28 +0300

    Improve utils.BreakInIndexes()'s description

--- utils/indexes_test.go
+++ utils/indexes_test.go
@@ -22,6 +22,7 @@ var breakInIndexesMatrix = []testCase{
 	{start: 50, end: 99, partSize: 50, result: []uint32{1}},
 	{start: 50, end: 50, partSize: 50, result: []uint32{1}},
 	{start: 50, end: 49, partSize: 50, result: []uint32{}},
+	{start: 0, end: 3, partSize: 1, result: []uint32{0, 1, 2, 3}},
 }
 
 func TestBreakInIndexes(t *testing.T) {

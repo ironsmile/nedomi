41f91640d59645990d3435dc93ae49691602bc37
Nedyalko Andreev <nedyalko.andreev@gmail.com>
2015-10-05 15:45:19 +0300

    Reduce cyclomatic complexity

--- utils/httputils/request_range.go
+++ utils/httputils/request_range.go
@@ -24,40 +24,6 @@ func (r Range) Range() string {
 	return fmt.Sprintf("bytes=%d-%d", r.Start, r.Start+r.Length-1)
 }
 
-// parseReqByteRange parses a byte range as per RFC 7233, section 2.1.
-func parseReqByteRange(start, end string, size uint64) (*Range, error) {
-	if start == "" {
-		// If no start is specified, end specifies the
-		// range starts relative to the end of the file.
-		ei, err := strconv.ParseUint(end, 10, 64)
-		if err != nil || ei == 0 {
-			return nil, errors.New("invalid range")
-		}
-		if ei > size {
-			ei = size
-		}
-		return &Range{Start: size - ei, Length: ei}, nil
-	}
-
-	si, err := strconv.ParseUint(start, 10, 64)
-	if err != nil || si >= size {
-		return nil, errors.New("invalid range")
-	}
-	if end == "" {
-		// If no end is specified, range extends to end of the file.
-		return &Range{Start: si, Length: size - si}, nil
-	}
-
-	ei, err := strconv.ParseUint(end, 10, 64)
-	if err != nil || si > ei {
-		return nil, errors.New("invalid range")
-	}
-	if ei >= size {
-		ei = size - 1
-	}
-	return &Range{Start: si, Length: ei - si + 1}, nil
-}
-
 // ParseRequestRange parses a client "Range" header string as per RFC 7233.
 func ParseRequestRange(s string, size uint64) ([]Range, error) {
 	if s == "" {
@@ -92,4 +58,43 @@ func ParseRequestRange(s string, size uint64) ([]Range, error) {
 		return nil, errors.New("invalid range")
 	}
 	return ranges, nil
+}
+
+// parseReqByteRange parses a byte range as per RFC 7233, section 2.1.
+func parseReqByteRange(start, end string, size uint64) (*Range, error) {
+	if start == "" {
+		// If no start is specified, end specifies the
+		// range starts relative to the end of the file.
+		return parseEndRelativeRange(end, size)
+	}
+
+	si, err := strconv.ParseUint(start, 10, 64)
+	if err != nil || si >= size {
+		return nil, errors.New("invalid range")
+	}
+	if end == "" {
+		// If no end is specified, range extends to end of the file.
+		return &Range{Start: si, Length: size - si}, nil
+	}
+
+	ei, err := strconv.ParseUint(end, 10, 64)
+	if err != nil || si > ei {
+		return nil, errors.New("invalid range")
+	}
+	if ei >= size {
+		ei = size - 1
+	}
+	return &Range{Start: si, Length: ei - si + 1}, nil
+}
+
+// parseEndRelativeRange handles ranges that use `suffix-byte-range-spec`
+func parseEndRelativeRange(end string, size uint64) (*Range, error) {
+	ei, err := strconv.ParseUint(end, 10, 64)
+	if err != nil || ei == 0 {
+		return nil, errors.New("invalid range")
+	}
+	if ei > size {
+		ei = size
+	}
+	return &Range{Start: size - ei, Length: ei}, nil
 }

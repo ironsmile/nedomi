91e53a722a99fb1ad98440574c8ddd77a06b4b1b
Nedyalko Andreev <nedyalko.andreev@gmail.com>
2015-10-08 11:18:01 +0300

    Fix a bug in Discard from mock storage

--- storage/mock.go
+++ storage/mock.go
@@ -106,16 +106,14 @@ func (s *MockStorage) Discard(id *types.ObjectID) error {
 		return os.ErrNotExist
 	}
 	delete(s.Objects, id.Hash())
-	if _, ok := s.Parts[id.Hash()]; !ok {
-		return os.ErrNotExist
-	}
 	delete(s.Parts, id.Hash())
+
 	return nil
 }
 
 // DiscardPart removes the specified part of the object.
 func (s *MockStorage) DiscardPart(idx *types.ObjectIndex) error {
-	if obj, ok := s.Parts[idx.ObjID.Hash()]; !ok {
+	if obj, ok := s.Parts[idx.ObjID.Hash()]; ok {
 		delete(obj, idx.Part)
 		return nil
 	}

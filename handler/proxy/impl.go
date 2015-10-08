1156ee4c09e25576f53d0cb01516b91db3c810cc
Nedyalko Andreev <nedyalko.andreev@gmail.com>
2015-10-08 18:34:56 +0300

    Import most of the code of stdlib's ReverseProxy

--- handler/proxy/impl.go
+++ handler/proxy/impl.go
@@ -1,66 +1,163 @@
 package proxy
 
 import (
-	"fmt"
+	"io"
 	"net"
 	"net/http"
-	"net/http/httputil"
+	"strings"
 	"time"
 
-	"golang.org/x/net/context"
-
-	"github.com/ironsmile/nedomi/config"
 	"github.com/ironsmile/nedomi/types"
+	"github.com/ironsmile/nedomi/utils/httputils"
+
+	"golang.org/x/net/context"
 )
 
-// Settings contains the possible settings for the proxy
-type Settings struct {
+// This code is based on the source of ReverseProxy from Go's standard library:
+// https://golang.org/src/net/http/httputil/reverseproxy.go
+// Copyright 2011 The Go Authors.
+
+// ReverseProxy is an HTTP Handler that takes an incoming request and
+// sends it to another server, proxying the response back to the
+// client.
+type ReverseProxy struct {
+	// Director must be a function which modifies
+	// the request into a new request to be sent
+	// using Transport. Its response is then copied
+	// back to the original client unmodified.
+	Director func(*http.Request)
+
+	// The transport used to perform proxy requests.
+	// If nil, http.DefaultTransport is used.
+	Transport http.RoundTripper
+
+	// FlushInterval specifies the flush interval
+	// to flush to the client while copying the
+	// response body.
+	// If zero, no periodic flushing is done.
+	FlushInterval time.Duration
+
+	// Logger specifies a logger for errors that occur when attempting
+	// to proxy the request.
+	Logger types.Logger
 }
 
-// Proxy is a simple reverse proxy
-type Proxy struct {
-	*httputil.ReverseProxy
-}
-
-// New returns a configured and ready to use Upstream instance.
-func New(cfg *config.Handler, l *types.Location, next types.RequestHandler) (*Proxy, error) {
-	//!TODO: get this from the config
-	if l.UpstreamAddress == nil {
-		return nil, fmt.Errorf("No upstream address for proxy handler in %s", l.Name)
-	}
-
-	//!TODO: get all of these hardcoded values from the config
-	director := func(req *http.Request) {
-		req.URL.Scheme = l.UpstreamAddress.Scheme
-		req.URL.Host = l.UpstreamAddress.Host
-		req.Host = l.UpstreamAddress.Host
-
-		// If we don't set it, Go sets it for us to something stupid...
-		req.Header.Set("User-Agent", "nedomi")
-	}
-
-	transport := &http.Transport{
-		Proxy: http.ProxyFromEnvironment,
-		Dial: (&net.Dialer{
-			Timeout:   10 * time.Second,
-			KeepAlive: 10 * time.Second,
-		}).Dial,
-		TLSHandshakeTimeout: 5 * time.Second,
-		DisableKeepAlives:   false,
-		DisableCompression:  true,
-		MaxIdleConnsPerHost: 5,
-	}
-
-	//!TODO: record statistics (times, errors, etc.) for all requests
-	return &Proxy{&httputil.ReverseProxy{
-		FlushInterval: 200 * time.Millisecond,
-		Director:      director,
-		Transport:     transport,
-		//!TODO: set ErrorLog to our own logger
-	}}, nil
-}
-
-// RequestHandle implements the interface
-func (p *Proxy) RequestHandle(_ context.Context, w http.ResponseWriter, r *http.Request) {
+// RequestHandle implements the nedomi interface
+func (p *ReverseProxy) RequestHandle(_ context.Context, w http.ResponseWriter, r *http.Request) {
 	p.ServeHTTP(w, r)
+}
+
+// Hop-by-hop headers. These are removed when sent to the backend.
+var hopHeaders = httputils.GetHopByHopHeaders()
+
+type requestCanceler interface {
+	CancelRequest(*http.Request)
+}
+
+type runOnFirstRead struct {
+	io.Reader // optional; nil means empty body
+
+	fn func() // Run before first Read, then set to nil
+}
+
+func (c *runOnFirstRead) Read(bs []byte) (int, error) {
+	if c.fn != nil {
+		c.fn()
+		c.fn = nil
+	}
+	if c.Reader == nil {
+		return 0, io.EOF
+	}
+	return c.Reader.Read(bs)
+}
+
+func (p *ReverseProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	transport := p.Transport
+	if transport == nil {
+		transport = http.DefaultTransport
+	}
+
+	outreq := new(http.Request)
+	*outreq = *req // includes shallow copies of maps, but okay
+	httputils.CopyHeadersWithout(req.Header, outreq.Header, hopHeaders...)
+
+	if closeNotifier, ok := rw.(http.CloseNotifier); ok {
+		if requestCanceler, ok := transport.(requestCanceler); ok {
+			reqDone := make(chan struct{})
+			defer close(reqDone)
+
+			clientGone := closeNotifier.CloseNotify()
+
+			outreq.Body = struct {
+				io.Reader
+				io.Closer
+			}{
+				Reader: &runOnFirstRead{
+					Reader: outreq.Body,
+					fn: func() {
+						go func() {
+							select {
+							case <-clientGone:
+								requestCanceler.CancelRequest(outreq)
+							case <-reqDone:
+							}
+						}()
+					},
+				},
+				Closer: outreq.Body,
+			}
+		}
+	}
+
+	p.Director(outreq)
+	outreq.Proto = "HTTP/1.1"
+	outreq.ProtoMajor = 1
+	outreq.ProtoMinor = 1
+	outreq.Close = false
+
+	if clientIP, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
+		// If we aren't the first proxy retain prior
+		// X-Forwarded-For information as a comma+space
+		// separated list and fold multiple headers into one.
+		if prior, ok := outreq.Header["X-Forwarded-For"]; ok {
+			clientIP = strings.Join(prior, ", ") + ", " + clientIP
+		}
+		outreq.Header.Set("X-Forwarded-For", clientIP)
+	}
+
+	res, err := transport.RoundTrip(outreq)
+	if err != nil {
+		p.Logger.Logf("http: proxy error: %v", err)
+		rw.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	for _, h := range hopHeaders {
+		res.Header.Del(h)
+	}
+
+	httputils.CopyHeaders(res.Header, rw.Header())
+
+	// The "Trailer" header isn't included in the Transport's response,
+	// at least for *http.Transport. Build it up from Trailer.
+	if len(res.Trailer) > 0 {
+		var trailerKeys []string
+		for k := range res.Trailer {
+			trailerKeys = append(trailerKeys, k)
+		}
+		rw.Header().Add("Trailer", strings.Join(trailerKeys, ", "))
+	}
+
+	rw.WriteHeader(res.StatusCode)
+	if len(res.Trailer) > 0 {
+		// Force chunking if we saw a response trailer.
+		// This prevents net/http from calculating the length for short
+		// bodies and adding a Content-Length.
+		if fl, ok := rw.(http.Flusher); ok {
+			fl.Flush()
+		}
+	}
+	io.Copy(rw, res.Body)
+	res.Body.Close() // close now, instead of defer, to populate res.Trailer
+	httputils.CopyHeaders(res.Trailer, rw.Header())
 }
---
+++ handler/proxy/new.go
@@ -0,0 +1,53 @@
+package proxy
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/ironsmile/nedomi/config"
+	"github.com/ironsmile/nedomi/types"
+)
+
+// Settings contains the possible settings for the proxy
+type Settings struct {
+}
+
+// New returns a configured and ready to use Upstream instance.
+func New(cfg *config.Handler, l *types.Location, next types.RequestHandler) (*ReverseProxy, error) {
+	//!TODO: get this from the config
+	if l.UpstreamAddress == nil {
+		return nil, fmt.Errorf("No upstream address for proxy handler in %s", l.Name)
+	}
+
+	//!TODO: get all of these hardcoded values from the config
+	director := func(req *http.Request) {
+		req.URL.Scheme = l.UpstreamAddress.Scheme
+		req.URL.Host = l.UpstreamAddress.Host
+		req.Host = l.UpstreamAddress.Host
+
+		// If we don't set it, Go sets it for us to something stupid...
+		req.Header.Set("User-Agent", "nedomi")
+	}
+
+	transport := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		Dial: (&net.Dialer{
+			Timeout:   10 * time.Second,
+			KeepAlive: 10 * time.Second,
+		}).Dial,
+		TLSHandshakeTimeout: 5 * time.Second,
+		DisableKeepAlives:   false,
+		DisableCompression:  true,
+		MaxIdleConnsPerHost: 5,
+	}
+
+	//!TODO: record statistics (times, errors, etc.) for all requests
+	return &ReverseProxy{
+		FlushInterval: 200 * time.Millisecond,
+		Director:      director,
+		Transport:     transport,
+		//!TODO: set ErrorLog to our own logger
+	}, nil
+}

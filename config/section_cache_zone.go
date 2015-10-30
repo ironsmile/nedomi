1ca43d70347b29b7ec5e8c04a8a36caf60105625
Mihail Stoykov <M.Stoikov@gmail.com>
2015-10-30 17:16:18 +0200

    add option to not prepend the cache key infront of file paths

--- config/section_cache_zone.go
+++ config/section_cache_zone.go
@@ -8,14 +8,15 @@ import (
 
 // CacheZone contains all configuration options for cache zones.
 type CacheZone struct {
-	ID                string
-	Type              string          `json:"type"`
-	Path              string          `json:"path"`
-	StorageObjects    uint64          `json:"storage_objects"`
-	PartSize          types.BytesSize `json:"part_size"`
-	Algorithm         string          `json:"cache_algorithm"`
-	BulkRemoveCount   uint64          `json:"bulk_remove_count"`
-	BulkRemoveTimeout uint64          `json:"bulk_remove_timeout"`
+	ID                 string
+	Type               string          `json:"type"`
+	Path               string          `json:"path"`
+	StorageObjects     uint64          `json:"storage_objects"`
+	PartSize           types.BytesSize `json:"part_size"`
+	Algorithm          string          `json:"cache_algorithm"`
+	BulkRemoveCount    uint64          `json:"bulk_remove_count"`
+	BulkRemoveTimeout  uint64          `json:"bulk_remove_timeout"`
+	SkipCacheKeyInPath bool            `json:"skip_cache_key_in_path"`
 }
 
 // Validate checks a CacheZone config section for errors.

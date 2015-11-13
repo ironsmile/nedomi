a24bbcc790a4e00dc86263f1fb84e891bc6b6db6
Mihail Stoykov <M.Stoikov@gmail.com>
2015-11-13 15:52:28 +0200

    add HTTP.io_transfer_size setting

--- utils/netutils/timeout_listener.go
+++ utils/netutils/timeout_listener.go
@@ -1,27 +1,47 @@
 package netutils
 
-import "net"
+import (
+	"net"
+	"sync"
+)
 
 type timeoutConnListener struct {
 	net.Listener
+	sizeOfTransfer int64
+	pool           sync.Pool
 }
 
-// DeadlineToTimeoutListener wraps the provided listener in a new one
+// DeadlineToTimeoutListenerConstructor returns a function that wraps
+// a provided listener in a new one
 // whose Accept methods returns a wrapped net.Conn whose Deadlines set
 // timeouts for each Read|Write individually.
 // Example: a conn.SetReadDeadline(time.Now().Add(time.Second)) will set a timeout
 // of one second. With the standard conn|listener this will mean that if you start reading a response
 // calling Read multiple times but it all takes more than a second it will timeout. With a connection
 // from this listener if each call to Read finishes in less than a second the connection will not timeout.
-func DeadlineToTimeoutListener(l net.Listener) net.Listener {
-	return &timeoutConnListener{Listener: l}
+// The sizeOfTransfer argument has the meaning of the size of transfer for each deadline set not for the whole connection.
+func DeadlineToTimeoutListenerConstructor(sizeOfTransfer int64) func(l net.Listener) net.Listener {
+	var pool = sync.Pool{
+		New: func() interface{} {
+			b := make([]byte, sizeOfTransfer)
+			return &b
+		},
+	}
+
+	return func(l net.Listener) net.Listener {
+		return &timeoutConnListener{
+			pool:           pool,
+			Listener:       l,
+			sizeOfTransfer: sizeOfTransfer,
+		}
+	}
 }
 
 // Accept calls the underlying accept and wraps the connection if not nil in timeouting connection
 func (t *timeoutConnListener) Accept() (net.Conn, error) {
 	conn, err := t.Listener.Accept()
 	if conn != nil {
-		conn = newTimeoutConn(conn)
+		conn = newTimeoutConn(conn, t.sizeOfTransfer, t.pool)
 	}
 
 	return conn, err

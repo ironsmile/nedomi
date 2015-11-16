393b3d8f4c31706e4f7f5440df04992ead863566
Mihail Stoykov <M.Stoikov@gmail.com>
2015-11-16 18:58:15 +0200

    add benchmarks of getContents

--- handler/cache/bench_test.go
+++ handler/cache/bench_test.go
@@ -2,7 +2,14 @@ package cache
 
 import (
 	"math/rand"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"golang.org/x/net/context"
+
+	"github.com/ironsmile/nedomi/utils"
+	"github.com/ironsmile/nedomi/utils/testutils"
 )
 
 func BenchmarkStorageSimultaneousRangeGetsFillingUp(b *testing.B) {
@@ -23,4 +30,53 @@ func BenchmarkStorageSimultaneousRangeGetsFillingUp(b *testing.B) {
 			testfunc(i)
 		}
 	})
+}
+
+func benchmarkGetContentsiOf40(b *testing.B, i int) {
+	app := newTestApp(b)
+	defer app.cleanup()
+	var file = "40parts"
+	var partSize = app.cacheHandler.Cache.Storage.PartSize()
+	app.fsmap[file] = testutils.GenerateMeAString(1, int64(40*partSize))
+
+	app.testRange(file, 0, partSize)
+	req, err := http.NewRequest("GET", "http://example.com/"+file, nil)
+	if err != nil {
+		panic(err)
+	}
+	var resp = httptest.NewRecorder()
+
+	objID := app.cacheHandler.NewObjectIDForURL(req.URL)
+	indexes := utils.BreakInIndexes(
+		objID, 0,
+		40*partSize-1,
+		partSize)
+	ctx := context.Background()
+
+	testfunc := func() {
+		rq := &reqHandler{app.cacheHandler, ctx, req, resp, objID, nil}
+		rq.obj, _ = rq.Cache.Storage.GetMetadata(rq.objID)
+		contents, _, err := rq.getContents(indexes, i)
+		if err != nil {
+			b.Fatal(err)
+		}
+		if err := contents.Close(); err != nil {
+			b.Fatal(err)
+		}
+	}
+
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			testfunc()
+		}
+	})
+}
+
+func BenchmarkGetContents0Of40(b *testing.B) {
+	benchmarkGetContentsiOf40(b, 0)
+}
+
+func BenchmarkGetContents1Of40(b *testing.B) {
+	benchmarkGetContentsiOf40(b, 1)
 }
